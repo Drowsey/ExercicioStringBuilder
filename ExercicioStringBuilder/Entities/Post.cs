ae81b97f00108fd6b76a4428a82002478fed4b2d
Allen Cardoso <allenkevin.cardoso@gmail.com>
2021-07-21 19:06:48 -0300

    Projeto Iniciado

---
+++ ExercicioStringBuilder/Entities/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioStringBuilder.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        List<Comment> comment = new List<Comment>();
+
+        public Post()
+        {
+        }
+
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+    }
+}
---
+++ ExercicioStringBuilder/Program.cs
@@ -0,0 +1,13 @@
+﻿using ExercicioStringBuilder.Entities;
+using System;
+
+namespace ExercicioStringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post post1 = new Post();
+        }
+    }
+}
